d611d2be5c7ed4b080de707fca2675a83e3097f2
jaydeeparts <email2jaydeeparts@gmail.com>
2025-06-19 00:06:57 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,6 +1,13 @@
 // React frontend entry point
 
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Backend server is running!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
