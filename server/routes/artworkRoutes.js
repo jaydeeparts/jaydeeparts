3c9f44fd86a3357e447127385348fbebdbceb69c
jaydeeparts <email2jaydeeparts@gmail.com>
2025-06-21 23:41:09 +0530

    Update artworkRoutes.js

--- server/routes/artworkRoutes.js
+++ server/routes/artworkRoutes.js
@@ -1,1 +1,29 @@
+// jaydeeparts/server/routes/artworkRoutes.js
 
+const express = require('express');
+const router = express.Router();
+
+// Dummy sample artworks (You can connect to MongoDB later)
+const sampleArtworks = [
+  {
+    _id: "1",
+    title: "Sunset Over Hills",
+    description: "A beautiful sunset over rolling hills.",
+    image: "https://via.placeholder.com/300",
+    price: 1999
+  },
+  {
+    _id: "2",
+    title: "Abstract Dream",
+    description: "Colorful abstract expression of a dreamscape.",
+    image: "https://via.placeholder.com/300",
+    price: 2499
+  }
+];
+
+// GET all artworks
+router.get('/', (req, res) => {
+  res.json(sampleArtworks);
+});
+
+module.exports = router;
